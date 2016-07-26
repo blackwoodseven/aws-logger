2cc6d1874f9a3a79cf466da717f18de776e9a7d5
Guillem Salas <gs@blackwoodseven.com>
2016-07-26 12:50:24 +0200

    split singleton configuration and logger logic into different files

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-// Create a unique symbol, so we can survive multiple instances
-// of different versions due npm subdependencies
-// More information on:
-// https://derickbailey.com/2016/03/09/creating-a-true-singleton-in-node-js-with-es6-symbols/
-const AWS_LOGGER_STATE_KEY = Symbol.for('com.blackwoodseven.aws-logger')
-
-//Check if logger has been initialized before
-var globalSymbols = Object.getOwnPropertySymbols(global);
-var hasAwsLogger = globalSymbols.indexOf(AWS_LOGGER_STATE_KEY) > -1;
-
-const initializeGlobalState = () => {
-  return global[AWS_LOGGER_STATE_KEY] = {
-    currentEnvrionment: null, disableLogging: false
-  }
-}
-// grab the configuration from the global state
-const state = hasAwsLogger ?
-                global[AWS_LOGGER_STATE_KEY]
-                : initializeGlobalState()
+const config = require('./singleton-configuration');
 
 const concatToArguments = (logLevel, args) => {
-  if (state.currentEnvrionment == null) {
+  if (config.currentEnvrionment == null) {
     console.warn('[Warn] Missing environment for logging, please call setEnvironment before using the aws-logger.')
   }
-  return [`[${logLevel}]`, `[${state.currentEnvrionment}]`].concat(Array.from(args));
+  return [`[${logLevel}]`, `[${config.currentEnvrionment}]`].concat(Array.from(args));
 }
 
 const createLoggerFn = (logLevel, method) => function() {
-  return state.disableLogging || console[method].apply(console, concatToArguments(logLevel,arguments))
+  return config.disableLogging || console[method].apply(console, concatToArguments(logLevel,arguments))
 }
 
 module.exports = {
-  setEnvironment: (env) => state.currentEnvrionment = env,
-  disableLogging: () => { state.disableLogging = true },
+  setEnvironment: (env) => config.currentEnvrionment = env,
+  disableLogging: () => { config.disableLogging = true },
   log: createLoggerFn('Info','log'),
   info: createLoggerFn('Info','info'),
   warn: createLoggerFn('Warn','warn'),
---
+++ singleton-configuration.js
@@ -0,0 +1,19 @@
+// Create a unique symbol, so we can survive multiple instances
+// of different versions due npm subdependencies
+// More information on:
+// https://derickbailey.com/2016/03/09/creating-a-true-singleton-in-node-js-with-es6-symbols/
+const AWS_LOGGER_STATE_KEY = Symbol.for('com.blackwoodseven.aws-logger')
+
+//Check if logger has been initialized before
+var globalSymbols = Object.getOwnPropertySymbols(global);
+var hasAwsLogger = globalSymbols.indexOf(AWS_LOGGER_STATE_KEY) > -1;
+
+const initializeGlobalState = () => {
+  return global[AWS_LOGGER_STATE_KEY] = {
+    currentEnvrionment: null, disableLogging: false
+  }
+}
+// grab the configuration from the global state
+module.exports = hasAwsLogger ?
+                global[AWS_LOGGER_STATE_KEY]
+                : initializeGlobalState()
